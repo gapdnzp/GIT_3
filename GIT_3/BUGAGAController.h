677303736ab8a6a3c3b5650102f6020d83920895
Горяинов Александр <gap@MacBook-Pro-Gorainov.local>
2017-06-11 11:34:37 +0300

    add new controller

---
+++ GIT_3/BUGAGAController.h
@@ -0,0 +1,13 @@
+//
+//  BUGAGAController.h
+//  GIT_3
+//
+//  Created by Горяинов Александр on 11.06.17.
+//  Copyright © 2017 GAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BUGAGAController : UIViewController
+
+@end
---
+++ GIT_3/BUGAGAController.m
@@ -0,0 +1,37 @@
+//
+//  BUGAGAController.m
+//  GIT_3
+//
+//  Created by Горяинов Александр on 11.06.17.
+//  Copyright © 2017 GAP. All rights reserved.
+//
+
+#import "BUGAGAController.h"
+
+@interface BUGAGAController ()
+
+@end
+
+@implementation BUGAGAController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
